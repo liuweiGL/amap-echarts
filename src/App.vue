f0c5f8d7cc875bfa54de6a84cab35909df1e5cf8
liuwei <wei.liu@pcsg.net>
2019-08-19 18:22:26 +0800

    feat(开发): 修改 demo

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,107 @@
 <template>
-  <div id="app">
-    <effect-scatter />
-  </div>
+  <el-amap
+    :events="events"
+    :zoom="zoom"
+    :center="center"
+    :map-style="mapStyle"
+  />
 </template>
 
 <script>
-import EffectScatter from '@components/Lines'
+import 'echarts/lib/chart/line'
+import 'echarts/lib/chart/lines'
+import 'echarts/lib/chart/effectScatter'
+import 'amap-echarts/dist/themes/macarons'
+import AMapEcharts from './libs/index'
+import countries from '@assets/data/countries'
+
+AMapEcharts.config({ theme: 'macarons' })
+
+const scatterData = []
+const linesData = []
+
+Object.keys(countries).forEach(key => {
+  scatterData.push({
+    name: key,
+    value: countries[key]
+  })
+
+  if (key !== '中国') {
+    linesData.push({
+      coords: [countries[key], countries['中国']]
+    })
+  }
+})
 
 export default {
   name: 'App',
-  components: {
-    EffectScatter
+  data() {
+    return {
+      zoom: 3,
+      center: countries['中国'],
+      mapStyle: 'amap://styles/whitesmoke',
+      events: {
+        init: map => {
+          this.createScatterChart(map)
+        }
+      }
+    }
+  },
+  beforeDestroy() {
+    if (this.ae) {
+      this.ae.dispose()
+      console.log('============ echarts 已销毁 ============ ')
+    }
+  },
+  methods: {
+    createScatterChart(map) {
+      this.ae = new AMapEcharts(map)
+      this.ae.setOption({
+        series: [
+          {
+            type: 'effectScatter',
+            coordinateSystem: 'amap',
+            zlevel: 1,
+            symbolSize: 8,
+            rippleEffect: {
+              period: 4,
+              brushType: 'fill',
+              scale: 4
+            },
+            label: {
+              show: true,
+              distance: 8,
+              position: 'bottom',
+              formatter: '{b}'
+            },
+            data: scatterData
+          },
+          {
+            type: 'lines',
+            coordinateSystem: 'amap',
+            zlevel: 2,
+            symbol: 'none',
+            silent: true,
+            effect: {
+              period: 4,
+              show: true,
+              symbol: 'triangle',
+              symbolSize: 5,
+              trailLength: 0.2
+            },
+            lineStyle: {
+              width: 1,
+              opacity: 1,
+              curveness: 0.3
+            },
+            data: linesData
+          }
+        ]
+      })
+    }
   }
 }
 </script>
-
 <style>
 html,
 body,

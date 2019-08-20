54632caa10e09863a39ea3c028f5007de3514966
liuwei <wei.liu@pcsg.net>
2019-08-20 15:12:28 +0800

    build: 优化 docs 发布脚本

--- scripts/docs.cmd
+++ scripts/docs.cmd
@@ -1,15 +1,16 @@
-:: 打包文档，命令没有返回成功码，导致后面的命令不执行？
-:: yarn docs:build
+@echo off
+REM 打包文档
+call yarn docs:build
 
 
-:: cd 到构建输出的目录下
-cd docs-dist
+REM cd 到构建输出的目录下
+call cd docs-dist
 
-git init
-git add -A
-git commit -m"docs: update"
+call git init
+call git add -A
+call git commit -m"docs: update"
 
-:: 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:liuweigl/amap-echarts.git master:gh-pages
+REM 部署到 https://<USERNAME>.github.io/<REPO>
+call git push -f https://github.com/liuweiGL/amap-echarts.git master:gh-pages
 
-cd ..
+call cd ..

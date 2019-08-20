e89a4c11611c0757805fc077de0c65fda62711c8
liuwei <wei.liu@pcsg.net>
2019-08-20 14:54:24 +0800

    build: 添加 git push 操作

--- scripts/npm.cmd
+++ scripts/npm.cmd
@@ -2,7 +2,7 @@
 REM 构建类库
 call yarn lib
 call git add dist
-call git commit -m"chore: 构建类库"
+call git commit -m"chore: 类库发布前构建"
 
 REM 更新版本
 call yarn release
@@ -11,3 +11,6 @@ REM 发布到 npm 仓库，需要事先登录
 call nrm use npm
 call npm publish
 call nrm use taobao
+
+REM 推送 git
+call git push origin --tags

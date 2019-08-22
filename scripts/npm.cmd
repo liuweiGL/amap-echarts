c5a4b921d2fb48aa41e827368de3904c2aaad5ab
liuweigl <wei2460@qq.com>
2019-08-22 17:01:09 +0800

    build: 修改发布脚本

--- scripts/npm.cmd
+++ scripts/npm.cmd
@@ -2,7 +2,7 @@
 REM 构建类库
 call yarn lib
 call git add dist
-call git commit -m"chore: 类库发布前构建"
+call git commit -m"chore: npm 发布前构建"
 
 REM 更新版本
 call yarn standard-version
@@ -13,9 +13,3 @@ call npm publish --registry="https://registry.npmjs.org/"
 
 REM 推送 git
 call git push origin --tags
-
-REM 合并 master
-call git checkout master
-call git merge dev
-call git push
-call git checkout dev
